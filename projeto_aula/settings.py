c0335099aeeb462597fe44e18f4c34cb3fb7ce74
Bryan Dos Santos <92175547+Bryandossantos@users.noreply.github.com>
2024-04-16 18:53:55 -0300

    correção

--- projeto_aula/settings.py
+++ projeto_aula/settings.py
@@ -111,7 +111,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
+
+DATE_FORMAT = "%Y-%m-%d"
+
+DATE_INPUT_FORMATS = ['%Y-%m-%d']
 
 TIME_ZONE = 'UTC'
 
